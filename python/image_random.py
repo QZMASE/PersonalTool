8abd7b1d25b835b2a9fab37305398b753d5484e7
不务正业的IT民工 <qzm_work@foxmail.com>
2019-02-10 19:53:57 +0800

    Update image_random.py

--- python/image_random.py
+++ python/image_random.py
@@ -3,8 +3,18 @@
 import os
 import random
 
-pt="./images/"
-image_name=os.listdir(pt)#linux下会丢弃前面的0，windows不会
-random.shuffle(image_name)
+image_name = os.listdir("./image/")#获取文件名
+num = round(len(image_name) * 0.2)#测试集数量，四舍五入取整
+random.shuffle(image_name)#打乱数据集
+
+#覆盖文件，若文件不存在自动创建
+train = open("train.txt", "w")
+test = open("test.txt", "w")
+
+n = 0#循环累计
 for temp in image_name:
-    print(temp.replace('.jpg',''))
+    n = n + 1
+    if n > num:
+        train.write(temp.replace('.jpg','') + "\n")
+    else:
+        test.write(temp.replace('.jpg','') + "\n")

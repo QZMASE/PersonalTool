e2f5fe5bcbc08d250ffb0af7e5841f887ef251b5
Ubuntu-NoteBook <qzm_work@foxmail.com>
2020-01-23 10:46:40 +0800

    修复bug

--- python/crawler_m3u8.py
+++ python/crawler_m3u8.py
@@ -13,25 +13,27 @@ class URL:
             Chrome/78.0.3904.97 Safari/537.36'
     }
     url = ""# 链接地址
-    htlm = ""# 链接内html内容
+    html = ""# 链接内html内容
     title = ""# 网页标题
     requests_flag = True# 请求标志位，默认请求成功True，请求失败False
     
     def __init__(self, url):
         self.url = url
 
-        self.html = self.requests_url(self.url).text
+        self.html = self.requests_url(self.url)
         if self.requests_flag == True:
+            self.html = self.html.text
             self.title = self.find(r'<title>+.+</title>')[0].replace("<title>", "").replace("</title>", "")
             
-            if not os.path.exists(self.down_dir):
-                os.mkdir(self.temp_dir)
+        if not os.path.exists(self.down_dir):
+            os.mkdir(self.temp_dir)
 
     # url.save("temp.html")
     def save(self, filename):
-        file = open(self.down_dir+filename, 'w', encoding='utf-8')
-        file.write(self.html)
-        file.close()
+        if self.requests_flag == True:
+            file = open(self.down_dir+filename, 'w', encoding='utf-8')
+            file.write(self.html)
+            file.close()
 
     # 返回正则表达式在字符串中所有匹配结果的列表
     def find(self, regex):
@@ -43,14 +45,14 @@ class URL:
         file = open(self.fail_file, mode='a+')
         file.write("%s\n" % self.url)
         file.close()
-        requests_flag = False
+        self.requests_flag = False
         print("请求失败！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！\n")
 
     def requests_url(self, url):
         try:
             text = requests.get(url, headers=self.headers, timeout=(10, 20))
         except:
-            requests_fail()
+            self.requests_fail()
             return None
         else:
             return text
@@ -62,20 +64,20 @@ class URL:
             m3u8_url = lists[0].split("=")[-1]# 第一层只有一个m3u8文件
             print("第一层地址", m3u8_url)
 
-            m3u8_text = self.requests_url(m3u8_url).text
+            m3u8_text = self.requests_url(m3u8_url)
             if self.requests_flag == True:
-                m3u8_text = m3u8_text.split('\n')
+                m3u8_text = m3u8_text.text.split('\n')
                 for m3u8_text_list in m3u8_text:
                     if ".m3u8" in m3u8_text_list:# 找到第二层m3u8文件地址
                         print("第二层地址", m3u8_text_list)
-                        url = "https://v.ff-cdn.com/" + m3u8_text_list# 合成地址
-                        # url = m3u8_url.replace("index.m3u8", "") + m3u8_text_list# 合成地址
+                        # url = "https://v.ff-cdn.com/" + m3u8_text_list# 合成地址
+                        url = m3u8_url.replace("index.m3u8", "") + m3u8_text_list# 合成地址
                         base_url = url.replace("index.m3u8", "")# 提取当前基础路径
 
                         text = self.requests_url(url)
                         if self.requests_flag == True:
                             if text.status_code != 200:
-                                requests_fail()
+                                self.requests_fail()
                             else:
                                 file = open(self.down_dir + self.title + ".m3u8", mode='a+')
                                 text = text.text.split('\n')
@@ -95,6 +97,6 @@ if __name__=='__main__':
     for line in lines:
         if "http" in line:
             url = URL(line)
-            url.get_m3u8(url.find(r'<iframe src="([^"]+\.m3u8)"'))# 爬取网页内m3u8地址，<iframe src="/packs/dplayer/?url=
+            url.get_m3u8(url.find(r'<iframe src="([^"]+\.m3u8)"'))# 爬取网页内m3u8地址
 
                 
